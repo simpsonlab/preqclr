1ec85ea5cdb3163fd2fc2b3d587c02f05be93df3
jopineda <joannaa.pineda@gmail.com>
2017-10-26 09:15:17 -0400

    Adjusted to handle multiple samples

--- preqc-lr-report.py
+++ preqc-lr-report.py
@@ -14,7 +14,7 @@ import json
 def main():
         # process input
         parser = argparse.ArgumentParser(description='Display Pre-QC Long Read report')
-        parser.add_argument('-i', '--input', action="store", required=True, dest="preqc_file", help="PreQC file")
+        parser.add_argument('-i', '--input', action="store", required=True, dest="preqc_file", nargs='+', help="preqclr file(s)")
         parser.add_argument('-o', '--output', action="store", dest="prefix", default="preqc-lr-output", help="Prefix for output pdf")
         args = parser.parse_args()
         
@@ -64,17 +64,31 @@ def create_report(output_prefix, preqclr_file):
         ax11 = fig1.add_subplot(325)
         ax12 = fig1.add_subplot(326)
 
+	read_lengths = dict()
+	per_coverage_avg_num_overlaps = dict()
+	per_read_overlap_count = dict()
+	estimated_coverage_via_avg_overlaps = dict()
+	read_lengths_estimated_cov = dict()
+	per_read_GC_content = dict()
+	num_matching_residues = dict()
 
-        # get reads and some read info from fasta file
-        with open(preqclr_file) as json_file:  
-    		data = json.load(json_file)
-		read_lengths = data['read_lengths']					# histogram
-		per_coverage_avg_num_overlaps = data['coverage_avg_num_overlaps']	# scatter plot
-		per_read_overlap_count = data['num_overlaps_per_read']			# histogram
-		estimated_coverage_via_avg_overlaps = data['estimated_coverage'] 	# histogram
-		read_lengths_estimated_cov = data['read_lengths_estimated_cov'] 	# scatter plot
-		per_read_GC_content = data['read_counts_per_GC_content'] 		# histogram
-		num_matching_residues = data['num_matching_residues']		# histogram
+	markers = ['o', 's', '^', 'p', '+']
+	colors = ['#db5461', '#70d6ff', '#df9a57', '#fde74c', '#cad178'] 
+	for sample_preqclr_file in preqclr_file:
+		# get sample name
+		sample = str(os.path.splitext(sample_preqclr_file)[0])
+		color = colors.pop(0)
+		marker = markers.pop(0)
+        	with open(sample_preqclr_file) as json_file:
+			data = json.load(json_file)
+			read_lengths[sample] = (color, data['read_lengths'], marker)
+			per_coverage_avg_num_overlaps[sample] = (color, data['coverage_avg_num_overlaps'], marker)		# scatter plot
+			per_read_overlap_count[sample] = (color, data['num_overlaps_per_read'], marker)			# histogram
+			estimated_coverage_via_avg_overlaps[sample] = (color, data['estimated_coverage'], marker) 		# histogram
+			read_lengths_estimated_cov[sample] = (color, data['read_lengths_estimated_cov'], marker) 		# scatter plot
+			per_read_GC_content[sample] = (color, data['read_counts_per_GC_content'], marker) 			# histogram
+			num_matching_residues[sample] = (color, data['num_matching_residues'], marker)			# histogram
+
 	plot_read_length_distribution(ax1, read_lengths, output_prefix)
         plot_average_overlaps_vs_coverage_distribution(ax2, per_coverage_avg_num_overlaps, output_prefix)
         plot_num_overlaps_per_read_distribution(ax3, per_read_overlap_count, output_prefix)
@@ -87,6 +101,48 @@ def create_report(output_prefix, preqclr_file):
 
         pp.close()
 
+class sample(object):
+	name = ""
+	color = ""
+	genome_size_estimate = ""
+	read_lengths = list()
+	per_coverage_avg_num_overlaps = list()
+	per_read_overlap_count = dict()
+	estimated_coverage_via_avg_overlaps = dict()
+	read_lengths_estimated_cov = list()
+	per_read_GC_content = list()
+	num_matching_residues = list()
+ 
+	def __init__(self, read_lengths, per_coverage_avg_num_overlaps, per_read_overlap_count, estimated_coverage_via_avg_overlaps, read_lengths_estimated_cov, per_read_GC_content, num_matching_residues):
+		self.read_lengths =  read_lengths
+		self.per_coverage_avg_num_overlaps = per_coverage_avg_num_overlaps
+		self.per_read_overlap_count = per_read_overlap_count
+		self.estimated_coverage_via_avg_overlaps = estimated_coverage_via_avg_overlaps
+		self.read_lengths_estimated_cov = read_lengths_estimated_cov
+		self.per_read_GC_content = per_read_GC_content
+		self.num_matching_residues = num_matching_residues
+	
+	def get_read_lengths(self):
+		return self.read_lengths
+
+	def get_per_coverage_avg_num_overlaps(self):
+		return self.per_coverage_avg_num_overlaps
+
+	def get_per_read_overlap_count(self):
+		return self.per_read_overlap_count
+
+	def get_estimated_coverage_via_avg_overlaps(self):
+		return self.estimated_coverage_via_avg_overlaps
+
+	def get_read_lengths_estimated_cov(self):
+		return self.read_lengths_estimated_cov
+
+	def get_per_read_GC_content(self):
+		return self.per_read_GC_content
+
+	def get_genome_size_estimate(self):
+		return self.genome_size_estimate
+
 def plot_read_length_distribution(ax, data, output_prefix):
         print "\n\n\n\n"
         print "Plotting read length distribution"
@@ -94,111 +150,182 @@ def plot_read_length_distribution(ax, data, output_prefix):
 
 	read_lengths = data
 
+	# get the maximum read length across all the samples
+	max_read_length = 0
+	for sample in read_lengths:
+		sample_name = sample
+		sample_color = read_lengths[sample][0]
+		sample_read_lengths = read_lengths[sample][1]
+		sample_max_read_length = max(sample_read_lengths)
+		if sample_max_read_length > max_read_length:
+			max_read_length = sample_max_read_length
         binwidth = 1000.0
-        bins = np.arange(0, max(read_lengths) + binwidth, binwidth)
-        ax.hist(read_lengths, bins=bins)
+        bins = np.arange(0, max_read_length + binwidth, binwidth)
+
+	# now start plotting
+	for sample in read_lengths:
+                sample_name = sample
+                sample_color = read_lengths[sample][0]
+                sample_read_lengths = read_lengths[sample][1]
+                ax.hist(sample_read_lengths, color=sample_color, label=sample_name, alpha=0.5, bins=bins)
+
         ax.set_title('Read length distribution')
         ax.set_xlabel('Read lengths (bps)')
         ax.set_ylabel('Frequency')
         ax.grid(True, linestyle='-', linewidth=0.3)
 	ax.get_xaxis().get_major_formatter().set_scientific(False)
 	ax.get_xaxis().get_major_formatter().set_useOffset(False)
+        ax.legend(loc='upper right')
 
 def plot_average_overlaps_vs_coverage_distribution(ax, data, output_prefix):
         print "\n\n\n\n"
         print "Plotting average overlaps vs coverage"
         print "___________________________________________"
 
-	# sorted by key, return a list of tuples
-	lists = sorted(data.items())
-	# unpack a list of pairs into two tuples
-	x, y = zip(*lists)
+	for sample in data:
+		sample_name = sample
+		sample_color = data[sample][0]
+		sample_marker = data[sample][2]
+		sample_data = data[sample][1]
+		# sorted by key, return a list of tuples
+		lists = sorted(sample_data.items())
+		# unpack a list of pairs into two tuples
+		x, y = zip(*lists)
+        	ax.scatter(x, y, color=sample_color, marker=sample_marker, alpha=0.6, label=sample_name)
 
-        ax.scatter(x, y)
 	ax.set_title('Average number of overlaps vs coverage')
 	ax.set_xlabel('Coverage')
 	ax.set_ylabel('Average number of overlaps')
 	ax.grid(True, linestyle='-', linewidth=0.3)
+        ax.legend(loc='upper right')
 
 def plot_num_overlaps_per_read_distribution(ax, data, output_prefix):
         print "\n\n\n\n"
         print "Plotting number of overlaps per reads"
         print "___________________________________________"
 
-	reads_overlap_count = data
+        max_num_overlaps = 0
+        for sample in data:
+                sample_data = data[sample][1]
+                sample_max_num_overlaps = max(sample_data.values())
+                if sample_max_num_overlaps > max_num_overlaps:
+                        max_num_overlaps = sample_max_num_overlaps
+        binwidth = 0.01
+        bins = np.arange(0, float(max_num_overlaps) + binwidth, binwidth)
+
+	# now start plotting
+        for sample in data:
+                sample_name = sample
+                sample_color = data[sample][0]
+                sample_data = data[sample][1]
+	        ax.hist(sample_data.values(), bins=bins, alpha=0.5, label=sample_name, color=sample_color)
 
         # plotting the number of overlaps/read
-        binwidth = 0.01
-        bins = np.arange(0, max(reads_overlap_count.values()) + binwidth, binwidth)
-        ax.hist(reads_overlap_count.values(), bins=bins)
         ax.set_title('Per-base coverage distribution')
         ax.set_xlabel('Coverage')
         ax.set_ylabel('Frequency')    
         ax.grid(True, linestyle='-', linewidth=0.3)
+        ax.legend(loc='upper right')
 
 def plot_estimated_coverage(ax, data, output_prefix):
 	print "\n\n\n\n"
 	print "Plotting average coverage per base per read"
 	print "___________________________________________"
 
-        # plotting the cov per base per read
-	# the json file has reads-reads info stored as a list with all the coverage levels for each read,
-	# whereas for the ref-reads info, it is already pre-counted....This is temporary.
-        reads_reads = data[1]
-	ref_reads = {}
-	for key in data[0].keys():
-		ref_reads[int(key)]= data[0][key]
+        max_cov = 0
+	binwidth = 0
+	num_bins = 0
+        for sample in data:
+                sample_data = data[sample][1]
+		sample_data_reads = sample_data[1]
+                sample_max_cov = max(sample_data_reads)
+                if sample_max_cov > max_cov:
+                        max_cov = sample_max_cov
+		        q25, q75 = np.percentile(sample_data_reads, [25, 75])
+        		IQR = float(q75 - q25)
+        		n = float(len(sample_data_reads))
+        		h = int(float(2 * IQR) / float(n ** (1/3)))
+        		binwidth = int(float(max(sample_data_reads)-min(sample_data_reads))/float(h))
+        		num_bins = int(float(max(sample_data_reads)-min(sample_data_reads))/binwidth)
+        
+	bins = np.arange(0, max_cov + binwidth, binwidth)
 
-	# adjusting bins
-	q25, q75 = np.percentile(reads_reads, [25, 75])
-	IQR = float(q75 - q25)
-	n = float(len(reads_reads))
-	h = int(float(2 * IQR) / float(n ** (1/3)))
-	binwidth = int(float(max(reads_reads)-min(reads_reads))/float(h))
-	num_bins = int(float(max(reads_reads)-min(reads_reads))/binwidth)
-	bins = np.arange(0, max(reads_reads) + binwidth, binwidth)
+	for sample in data:
+		sample_name = sample
+		sample_color = data[sample][0]
+                sample_data = data[sample][1]
+                sample_data_reads = sample_data[1]
+		sample_data_ref = sample_data[0]
 
-	# plotting
-	ax.hist(ref_reads.keys(), weights=ref_reads.values(), alpha=0.5, label="ref (cov)", bins=bins)
-	ax.hist(reads_reads, alpha=0.5, label="reads", bins=bins)
+		# add read-reference info
+        	for key in sample_data[0].keys():
+                	sample_data_ref[int(key)] = sample_data[0][key]
+		#ax.hist(sample_data_ref.keys(), weights=sample_data_ref.values(), alpha=0.3, label="ref", bins=bins)
+		ax.hist(sample_data_reads, alpha=0.5, color=sample_color, label=sample_name, bins=bins)
+
         ax.set_title('Estimated coverage distribution')
 	ax.grid(True, linestyle='-', linewidth=0.3)
-	ax.set_xticks(np.arange(0, max(reads_reads), int(5 * round(float( num_bins/2)/5))))
+	ax.set_xticks(np.arange(0, max_cov, int(5 * round(float( num_bins/2)/5))))
         ax.set_xlabel('Avg. number of overlaps per base per read')
         ax.set_ylabel('Frequency')   
 	ax.legend(loc='upper right')
 
 def plot_estimated_coverage_per_read(ax, data, output_prefix):
-	# get x and y values from list of tuples
-	x,y = zip(*data)
+
+        for sample in data:
+                sample_name = sample
+                sample_color = data[sample][0]
+                sample_data = data[sample][1]
+		sample_marker = data[sample][2]
+        	# get x and y values from list of tuples
+        	x,y = zip(*sample_data)
+	        ax.scatter(x, y, alpha=0.5, marker=sample_marker, s=4, edgecolors=sample_color, linewidth=0.5, facecolors='None')
+	
 	# scatter plot with read length on x axis, and estimted coverage for read on y axis
-	ax.scatter(x, y, alpha=0.4, marker='o', s=2, edgecolors='#DB5461', linewidth=0.2, facecolors='#DB5461')
         ax.set_title('Estimated coverage vs read length')
         ax.grid(True, linestyle='-', linewidth=0.3)
         ax.set_xlabel('Read length')
         ax.set_ylabel('Coverage')
+        ax.legend(loc='upper right')
 
 def plot_num_matching_residues(ax, data, output_prefix):
+
+	max_num_matching_residues = 0
+	for sample in data:
+                sample_data = data[sample][1]
+		max_sample_data = max(sample_data)	
+		if max_sample_data > max_num_matching_residues:
+			max_num_matching_residues = max_sample_data
+
         binwidth = 100.0
-        bins = np.arange(0, max(data) + binwidth, binwidth)
-	ax.hist(data, bins=bins)
+        bins = np.arange(0, max_num_matching_residues + binwidth, binwidth)
+
+        for sample in data:
+                sample_name = sample
+                sample_color = data[sample][0]
+                sample_data = data[sample][1]
+		ax.hist(sample_data, bins=bins, label=sample_name, color=sample_color)	
+
 	ax.set_title('Number of matching residues per overlap distributions')
         ax.grid(True, linestyle='-', linewidth=0.3)
         ax.set_xlabel('Number of matching residues')
         ax.set_ylabel('Frequency')
 
 def plot_per_read_GC_content(ax, data, output_prefix):
-	per_read_GC_content = {}
-	print "Per read GC content"
-	print data
-	for GC_content_level in data:
-		per_read_GC_content[int(float(GC_content_level))] = int(data[GC_content_level])
+        per_read_GC_content = {}
         binwidth = 1.0
-	ax.hist(per_read_GC_content.keys(), weights=per_read_GC_content.values(), bins=np.arange(0, 100, binwidth), alpha=0.6)
+	for sample in data:
+                sample_name = sample
+                sample_color = data[sample][0]
+                sample_data = data[sample][1]
+		for GC_content_level in sample_data:
+			per_read_GC_content[int(float(GC_content_level))] = int(sample_data[GC_content_level])
+		ax.hist(per_read_GC_content.keys(), weights=per_read_GC_content.values(), color=sample_color, bins=np.arange(0, 100, binwidth), alpha=0.6)
         ax.set_title('Per read GC content')
         ax.set_xlabel('% GC content')
         ax.set_ylabel('Frequency')
         ax.grid(True, linestyle='-', linewidth=0.3)
+        ax.legend(loc='upper right')
 
 def getshape(d):
     if isinstance(d, dict):
