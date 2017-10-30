cb2a300944326804cd44fb3167168878ec339827
jopineda <joannaa.pineda@gmail.com>
2017-10-30 15:53:13 -0400

    Removed per_coverage_avg_num_overlaps plot

--- preqc-lr-report.py
+++ preqc-lr-report.py
@@ -65,13 +65,14 @@ def create_report(output_prefix, preqclr_file):
         ax12 = fig1.add_subplot(326)
 
 	estimated_genome_sizes = dict()
-	read_lengths = dict()
+	per_read_read_length = dict()
 	per_coverage_avg_num_overlaps = dict()
 	per_read_overlap_count = dict()
-	estimated_coverage_via_avg_overlaps = dict()
+	per_read_estimated_coverage = dict()
 	read_lengths_estimated_cov = dict()
 	per_read_GC_content = dict()
 	num_matching_residues = dict()
+	overlap_accuracies = dict()
 
 	markers = ['s', 'o', '^', 'p', '+']
 	colors = ['#E84855', '#3C91E6', '#FFFD82', '#FF9B71', '#1B998B'] 
@@ -83,68 +84,28 @@ def create_report(output_prefix, preqclr_file):
 			data = json.load(json_file)
 			sample = data['sample_name']
 			estimated_genome_sizes[sample] = (color, data['estimated_genome_size'], marker)
-			read_lengths[sample] = (color, data['read_lengths'], marker)
-			per_coverage_avg_num_overlaps[sample] = (color, data['coverage_avg_num_overlaps'], marker)		# scatter plot
-			per_read_overlap_count[sample] = (color, data['num_overlaps_per_read'], marker)				# histogram
-			estimated_coverage_via_avg_overlaps[sample] = (color, data['estimated_coverage'], marker) 		# histogram
+			per_read_read_length[sample] = (color, data['per_read_read_length'], marker)
+			#per_coverage_avg_num_overlaps[sample] = (color, data['coverage_avg_num_overlaps'], marker)		# scatter plot
+			per_read_overlap_count[sample] = (color, data['per_read_overlap_count'], marker)				# histogram
+			per_read_estimated_coverage[sample] = (color, data['per_read_estimated_coverage'], marker) 		# histogram
 			read_lengths_estimated_cov[sample] = (color, data['read_lengths_estimated_cov'], marker) 		# scatter plot
 			per_read_GC_content[sample] = (color, data['read_counts_per_GC_content'], marker) 			# histogram
 			num_matching_residues[sample] = (color, data['num_matching_residues'], marker)				# histogram
+			overlap_accuracies[sample] = (color, data['overlap_accuracies'], marker)				# scatter plot
 
 	plot_estimated_genome_size(ax1, estimated_genome_sizes, output_prefix)
-	plot_read_length_distribution(ax2, read_lengths, output_prefix)
-        plot_average_overlaps_vs_coverage_distribution(ax3, per_coverage_avg_num_overlaps, output_prefix)
-        plot_num_overlaps_per_read_distribution(ax4, per_read_overlap_count, output_prefix)
-        plot_estimated_coverage(ax5, estimated_coverage_via_avg_overlaps, output_prefix)
-	plot_estimated_coverage_per_read(ax6, read_lengths_estimated_cov, output_prefix)
+	plot_read_length_distribution(ax2, per_read_read_length, output_prefix)
+        #plot_average_overlaps_vs_coverage_distribution(ax3, per_coverage_avg_num_overlaps, output_prefix)
+        plot_num_overlaps_per_read_distribution(ax3, per_read_overlap_count, output_prefix)
+        plot_estimated_coverage(ax4, per_read_estimated_coverage, output_prefix)
+	plot_estimated_coverage_vs_read_length(ax5, read_lengths_estimated_cov, output_prefix)
+	plot_mean_overlap_accuracies_per_read(ax6, overlap_accuracies, output_prefix)
 	plot_per_read_GC_content(ax7, per_read_GC_content, output_prefix)
 	plot_num_matching_residues(ax8, num_matching_residues, output_prefix)
-	fig.savefig(pp, format='pdf')
+	fig.savefig(pp, format='pdf', dpi=400)
         fig1.savefig(pp, format='pdf')
 
         pp.close()
-
-class sample(object):
-	name = ""
-	color = ""
-	genome_size_estimate = ""
-	read_lengths = list()
-	per_coverage_avg_num_overlaps = list()
-	per_read_overlap_count = dict()
-	estimated_coverage_via_avg_overlaps = dict()
-	read_lengths_estimated_cov = list()
-	per_read_GC_content = list()
-	num_matching_residues = list()
- 
-	def __init__(self, read_lengths, per_coverage_avg_num_overlaps, per_read_overlap_count, estimated_coverage_via_avg_overlaps, read_lengths_estimated_cov, per_read_GC_content, num_matching_residues):
-		self.read_lengths =  read_lengths
-		self.per_coverage_avg_num_overlaps = per_coverage_avg_num_overlaps
-		self.per_read_overlap_count = per_read_overlap_count
-		self.estimated_coverage_via_avg_overlaps = estimated_coverage_via_avg_overlaps
-		self.read_lengths_estimated_cov = read_lengths_estimated_cov
-		self.per_read_GC_content = per_read_GC_content
-		self.num_matching_residues = num_matching_residues
-	
-	def get_read_lengths(self):
-		return self.read_lengths
-
-	def get_per_coverage_avg_num_overlaps(self):
-		return self.per_coverage_avg_num_overlaps
-
-	def get_per_read_overlap_count(self):
-		return self.per_read_overlap_count
-
-	def get_estimated_coverage_via_avg_overlaps(self):
-		return self.estimated_coverage_via_avg_overlaps
-
-	def get_read_lengths_estimated_cov(self):
-		return self.read_lengths_estimated_cov
-
-	def get_per_read_GC_content(self):
-		return self.per_read_GC_content
-
-	def get_genome_size_estimate(self):
-		return self.genome_size_estimate
 
 def plot_read_length_distribution(ax, data, output_prefix):
         print "\n\n\n\n"
@@ -232,7 +193,7 @@ def plot_num_overlaps_per_read_distribution(ax, data, output_prefix):
 
 def plot_estimated_coverage(ax, data, output_prefix):
 	print "\n\n\n\n"
-	print "Plotting average coverage per base per read"
+	print "Plotting estimated coverage per read"
 	print "___________________________________________"
 
         max_cov = 0
@@ -240,40 +201,41 @@ def plot_estimated_coverage(ax, data, output_prefix):
 	num_bins = 0
         for sample in data:
                 sample_data = data[sample][1]
-		sample_data_reads = sample_data[1]
-                sample_max_cov = max(sample_data_reads)
-                if sample_max_cov > max_cov:
-                        max_cov = sample_max_cov
-		        q25, q75 = np.percentile(sample_data_reads, [25, 75])
-        		IQR = float(q75 - q25)
-        		n = float(len(sample_data_reads))
-        		h = int(float(2 * IQR) / float(n ** (1/3)))
-        		binwidth = int(float(max(sample_data_reads)-min(sample_data_reads))/float(h))
-        		num_bins = int(float(max(sample_data_reads)-min(sample_data_reads))/binwidth)
-        
-	bins = np.arange(0, max_cov + binwidth, binwidth)
-
+		sample_data_reads = sample_data[0]
+                sample_data_upperbound = sample_data[1]		# precalculated est. cov. upperbound 
+		sample_data_num_reads = sample_data[2]		# number of reads after filtering
+		sample_data_q25 = sample_data[3]		# 1st quartile cov after filtering
+		sample_data_q75 = sample_data[4]		# 3rd quartile cov after filtering
+                if sample_data_upperbound > max_cov:
+                        max_cov = sample_data_upperbound
+        		IQR = float(sample_data_q75 - sample_data_q25)
+        		n = float(sample_data_num_reads)
+        		binwidth = int(float(2 * IQR) / float(n ** (1/3)))
+			print binwidth
+        		num_bins = int(float(max_cov-min(sample_data_reads))/binwidth)
+        print max(sample_data_reads)
+	bins = np.arange(0, max_cov + binwidth, num_bins)
+	print num_bins
 	for sample in data:
 		sample_name = sample
 		sample_color = data[sample][0]
                 sample_data = data[sample][1]
-                sample_data_reads = sample_data[1]
-		sample_data_ref = sample_data[0]
+                sample_data_reads = sample_data[0]
+		sample_data_upperbound = sample_data[1]
 
-		# add read-reference info
-        	for key in sample_data[0].keys():
-                	sample_data_ref[int(key)] = sample_data[0][key]
-		#ax.hist(sample_data_ref.keys(), weights=sample_data_ref.values(), alpha=0.3, label="ref", bins=bins)
 		ax.hist(sample_data_reads, alpha=0.5, color=sample_color, label=sample_name, bins=bins)
 
-        ax.set_title('Coverage distribution')
+        ax.set_title('Estimated coverage distribution')
 	ax.grid(True, linestyle='-', linewidth=0.3)
-	ax.set_xticks(np.arange(0, max_cov, int(5 * round(float( num_bins/2)/5))))
-        ax.set_xlabel('Avg. number of overlaps per base per read')
+	ax.set_xticks(np.arange(0, max_cov + binwidth, num_bins*5.0))
+        ax.set_xlabel('Estimated coverage')
         ax.set_ylabel('Frequency')   
 	ax.legend(loc='upper right')
 
-def plot_estimated_coverage_per_read(ax, data, output_prefix):
+def plot_estimated_coverage_vs_read_length(ax, data, output_prefix):
+        print "\n\n\n\n"
+        print "Plotting estimated coverage vs read length scatter plot"
+        print "___________________________________________"
 
         for sample in data:
                 sample_name = sample
@@ -282,16 +244,20 @@ def plot_estimated_coverage_per_read(ax, data, output_prefix):
 		sample_marker = data[sample][2]
         	# get x and y values from list of tuples
         	x,y = zip(*sample_data)
-	        ax.scatter(x, y, alpha=0.5, marker=sample_marker, s=4, edgecolors=sample_color, linewidth=0.5, facecolors='None')
+	        ax.scatter(x, y, alpha=0.5, marker=sample_marker, s=4, edgecolors=sample_color, linewidth=0.5, facecolors='None', rasterized=True)
 	
 	# scatter plot with read length on x axis, and estimted coverage for read on y axis
-        ax.set_title('Coverage vs read length')
+        ax.set_title('Estimated coverage vs read length')
         ax.grid(True, linestyle='-', linewidth=0.3)
-        ax.set_xlabel('Read length')
-        ax.set_ylabel('Coverage')
+        ax.set_xlabel('Read length (bps)')
+        ax.set_ylabel('Estimated coverage')
         ax.legend(loc='upper right')
+	#ax.set_yscale('log')
 
 def plot_num_matching_residues(ax, data, output_prefix):
+        print "\n\n\n\n"
+        print "Plotting number of matching residues"
+        print "___________________________________________"
 
 	max_num_matching_residues = 0
 	for sample in data:
@@ -315,12 +281,17 @@ def plot_num_matching_residues(ax, data, output_prefix):
         ax.set_ylabel('Frequency')
 
 def plot_per_read_GC_content(ax, data, output_prefix):
+        print "\n\n\n\n"
+        print "Plotting GC content"
+        print "___________________________________________"
+
         per_read_GC_content = {}
         binwidth = 1.0
 	for sample in data:
                 sample_name = sample
                 sample_color = data[sample][0]
                 sample_data = data[sample][1]
+		print sample_data
 		for GC_content_level in sample_data:
 			per_read_GC_content[int(float(GC_content_level))] = int(sample_data[GC_content_level])
 		ax.hist(per_read_GC_content.keys(), weights=per_read_GC_content.values(), color=sample_color, bins=np.arange(0, 100, binwidth), alpha=0.6)
@@ -353,6 +324,13 @@ def plot_estimated_genome_size(ax, data, output_prefix):
         # plotting the number of overlaps/read
 	y_pos = np.arange(len(genome_sizes))
 	ax.bar(y_pos, genome_sizes, align='center', alpha=0.5, color=colors)
+	# add the actual estimated genome size values to the bars
+	rects = ax.patches
+	
+	for rect, value in zip(rects, genome_sizes):
+		height = rect.get_height()
+		ax.text(rect.get_x() + rect.get_width()/2, height + 5, int(value), ha='center', va='bottom', fontsize='5')
+
 	ax.set_xticks(y_pos)
 	ax.set_xticklabels(sample_names)
         ax.set_title('Estimated genome size')
@@ -360,7 +338,27 @@ def plot_estimated_genome_size(ax, data, output_prefix):
         ax.set_ylabel('Genome size (bps)')
         ax.grid(True, linestyle='-', linewidth=0.3)
         ax.legend(loc='upper right')
-	
+
+def plot_mean_overlap_accuracies_per_read(ax, data, output_prefix):
+        print "\n\n\n\n"
+        print "Plotting mean overlap accuracies per read vs read length"
+        print "___________________________________________"
+
+        for sample in data:
+                sample_name = sample
+                sample_color = data[sample][0]
+                sample_data = data[sample][1]
+                sample_marker = data[sample][2]
+                # get x and y values from list of tuples
+                y,x = zip(*sample_data)
+                ax.scatter(x, y, alpha=0.5, marker=sample_marker, s=4, edgecolors=sample_color, linewidth=0.5, facecolors='None', rasterized=True)
+
+        # scatter plot with read length on x axis, and estimted coverage for read on y axis
+        ax.set_title('Mean accuracy vs read length')
+        ax.grid(True, linestyle='-', linewidth=0.3)
+        ax.set_xlabel('Read length')
+        ax.set_ylabel('Mean accuracy')
+        ax.legend(loc='upper right')	
 
 def getshape(d):
 	if isinstance(d, dict):
