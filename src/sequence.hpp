b9d5f2a05df86bbeb86f41df5410544765414387
jopineda <joannaa.pineda@gmail.com>
2018-03-14 11:31:57 -0400

    remove read_id in sequence structure as it is redundant and new method for removing duplicate overlaps

--- src/sequence.hpp
+++ src/sequence.hpp
@@ -12,9 +12,8 @@ using namespace std;
 class sequence
 {
   public:
-    string read_id;
     unsigned long int read_len;
     double cov;
-    void set(string i, unsigned long int l, double c);
+    void set(unsigned long int l, double c);
     void updateCov(double c);
 };
